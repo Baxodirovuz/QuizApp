4aba24eb40fbe71c020ef7b11f9cd4300323b34f
Baxodirovuz <98306023+Baxodirovuz@users.noreply.github.com>
2022-06-21 15:30:45 +0500

    author

--- src/main/java/QuizApp.java
+++ src/main/java/QuizApp.java
@@ -1,8 +1,16 @@
 import Languages.MainLanguages;
 
+
+/***
+ * Jaloliddin Baxodirov
+ */
+
 public class QuizApp {
     public static void main(String[] args) {
         MainLanguages main = new MainLanguages();
         main.chooseLang();
+
+
+
     }
 }
